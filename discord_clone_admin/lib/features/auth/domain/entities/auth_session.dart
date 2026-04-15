25d5028f911ba9c85af5e4cc8818525a993a3e08
Nguyen Hoang Giap <nguyenhoanggiap2005@gmail.com>
2026-04-15 11:31:07 +0700

    fix

--- discord_clone_admin/lib/features/auth/domain/entities/auth_session.dart
+++ discord_clone_admin/lib/features/auth/domain/entities/auth_session.dart
@@ -1,13 +1,13 @@
 class AuthSession {
+  final String token;
+  final int userId;
+  final String userName;
+  final String message;
+
   const AuthSession({
     required this.token,
     required this.userId,
     required this.userName,
     required this.message,
   });
-
-  final String token;
-  final int userId;
-  final String userName;
-  final String message;
 }
